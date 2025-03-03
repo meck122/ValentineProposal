efa54d8c9d90554945dfe71a4e6c6f33210b4c03
Mark Liao <liaozhoudi@gmail.com>
2025-03-02 21:54:13 -0600

    nvm

--- src/pages/SuccessPage.tsx
+++ src/pages/SuccessPage.tsx
@@ -1,6 +1,6 @@
 import Confetti from "react-confetti";
 // import valentineImage from "../assets/valentine.svg";
-import us from "../src/assets/us.png";
+import us from "../assets/us.png";
 
 export default function SuccessPage() {
   return (
