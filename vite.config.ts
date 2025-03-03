97a6f4cfb5cc7fdffc538afe4775bff04e385f3c
Mark Liao <liaozhoudi@gmail.com>
2025-03-02 21:35:57 -0600

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ValetineProposal/',
+  base: '/ValentineProposal/',
 })
 
